75a7e1a7a24117e09d2b307ebddb601511e31ebd
Nilay Jain <nilayjain12@gmail.com>
2025-03-19 14:07:17 -0700

    using huggingface embedding model

--- app/chatbot/retriever.py
+++ app/chatbot/retriever.py
@@ -8,5 +8,5 @@ VECTOR_DB_PATH = BASE_DIR / "data" / "database" / "faiss_index"
 
 def load_vector_db():
     if os.path.exists(VECTOR_DB_PATH):
-        return FAISS.load_local(VECTOR_DB_PATH, HuggingFaceEmbeddings(model="all-MiniLM-L6-v2"), allow_dangerous_deserialization=True)
+        return FAISS.load_local(VECTOR_DB_PATH, HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2"), allow_dangerous_deserialization=True)
     return None
